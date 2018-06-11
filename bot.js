24ba652a3c2be89c39e28c5f8fc437187a0adb7d
hypixej <40128873+hypixej@users.noreply.github.com>
2018-06-11 21:50:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,10 @@ client.on('message', message => {
     if (message.content === 'alert') {
     	message.reply('**[BOT UPDATE]** - I am your new bot!');
   	}
+client.on('guildMemberAdd', user => {
+client.channels.get('454425518981513238').send('Welcome to my server, ' + user + '!')
+}
+          
 });
 
 // THIS  MUST  BE  THIS  WAY
