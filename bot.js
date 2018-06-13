939208823cd775853d030339265ce02eb389eb85
hypixej <40128873+hypixej@users.noreply.github.com>
2018-06-14 00:11:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,8 @@ client.on('message', message => {
 
 client.on("message", (message) => {
   if(message.content === "alert") {
-    message.channel.send("**[BOT UPDATE]** %% All Updates are going to be made by the bot now!");
+    message.channel.send("**[BOT UPDATE]**");
+    message.channel.send("All the bot updates will be made by the bot now!");
   }
 });
 client.on('guildMemberAdd' , member => {               //You can Change The Name
